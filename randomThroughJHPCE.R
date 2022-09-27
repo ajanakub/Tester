47ce228b75f4f22000b7f3deb8b059303ad180c2
ajanakub <bukolaajanaku@gmail.com>
2022-09-27 12:19:14 -0400

    adding the reproducibility info to file and pushing changes

--- randomThroughJHPCE.R
+++ randomThroughJHPCE.R
@@ -1,2 +1,14 @@
+library('sessioninfo')
+
+## Wait for 60 seconds (did this with Kristen, not on the video)
+Sys.sleep(60)
+
 rnorm(5)
 print("hi Bukola")
+
+## Reproducibility information
+print('Reproducibility information:')
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
